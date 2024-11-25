87d481bcfc716d298212df59c7b810b7ad12eacf
jungyoonn <sophia7625@naver.com>
2024-11-25 12:11:56 +0900

    마이바티스 init, 싱글톤 생성

---
+++ src/main/java/utils/MybatisInit.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MybatisInit {
+	private static MybatisInit init = new MybatisInit();
+	public static MybatisInit getInstance() {
+		return init;
+	}
+	
+	public SqlSessionFactory sqSessionFactory() {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		return sqlSessionFactory;
+	}
+}
