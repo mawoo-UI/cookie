9b45460db984e219107ec093e02692136aa91c72
jungyoonn <sophia7625@naver.com>
2024-11-25 15:58:41 +0900

    수강신청 servlet

---
+++ src/main/java/servlet/oneday/RegisterClass.java
@@ -0,0 +1,17 @@
+package servlet.oneday;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/oneday/regclass")
+public class RegisterClass extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/jsp/oneday/register-class.jsp").forward(req, resp);
+	}
+}
