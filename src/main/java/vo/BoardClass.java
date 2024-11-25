e41c2e097358e02738eb864f8967833442e4655e
jungyoonn <sophia7625@naver.com>
2024-11-25 13:14:47 +0900

    클래스 게시판 vo

---
+++ src/main/java/vo/BoardClass.java
@@ -0,0 +1,21 @@
+package vo;
+
+import java.util.Date;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardClass {
+	private Long cbno;
+	private String title;
+	private String content;
+	private String host;
+	private Long viewCount;
+	private Date regdate;
+	private Date updatedate;
+	private boolean end;
+	private boolean accept;
+}
