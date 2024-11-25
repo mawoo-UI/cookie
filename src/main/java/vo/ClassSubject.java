558d3cfc8c3a07a3da21b756b5b3df667bb2634c
jungyoonn <sophia7625@naver.com>
2024-11-25 13:15:14 +0900

    클래스 관리 vo

---
+++ src/main/java/vo/ClassSubject.java
@@ -0,0 +1,19 @@
+package vo;
+
+import java.util.Date;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClassSubject {
+	private Long sno;
+	private String sname;
+	private int maxEntry;
+	private int price;
+	private String location;
+	private Date regdate;
+	private Date updatedate;
+	private String starter;
+}
