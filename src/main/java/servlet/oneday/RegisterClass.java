47d67ac861a8b4ea967a10bb3ca74d3c473f57b8
jungyoonn <sophia7625@naver.com>
2024-11-29 15:36:17 +0900

    유효성 검사 후 db에 데이터 넣기 작업 완료

--- src/main/java/servlet/oneday/RegisterClass.java
+++ src/main/java/servlet/oneday/RegisterClass.java
@@ -19,6 +19,7 @@ import service.MyClassServiceImpl;
 import utils.Commons;
 import vo.ClassCurriculum;
 import vo.ClassReg;
+import vo.Member;
 import vo.MyClass;
 
 @WebServlet("/oneday/regclass")
@@ -30,40 +31,49 @@ public class RegisterClass extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Object memberObj = req.getSession().getAttribute("member");
+		
+		if(memberObj == null) {
+			Commons.printMsg("로그인이 필요한 서비스입니다.", "/cookie/member/signin", resp);
+			return;
+		}
+		Member m = (Member) memberObj;
+		
+		if(m.getName() == null) {
+			Commons.printMsg("실명 인증 후 이용하실 수 있습니다.", "/cookie/member/mypage", resp);
+			return;
+		}
+		
 		Long cbno = Long.valueOf(req.getParameter("cbno"));
 		req.setAttribute("regClass", boardClassService.findBy(cbno));
 		req.setAttribute("days", curriculumService.boardList(cbno));
-		System.out.println(req.getParameter("cbno"));
 		
 		req.getRequestDispatcher("/WEB-INF/jsp/oneday/register-class.jsp").forward(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Long cbno = Long.valueOf(req.getParameter("cbno"));
-		String content = req.getParameter("content");
-		String nickname = req.getParameter("nickname");
-		String tel = req.getParameter("tel");
+		Long cbno = Long.valueOf(req.getParameter("cbno")); // ccno 찾기
 		String startdate = req.getParameter("startdate"); // ccno 찾기
-		System.out.println(startdate);
 		
 		ClassCurriculum curriculum = curriculumService.findByStartdateAndCbno(startdate, cbno);
+		System.out.println(curriculum);
+		
 		Long ccno = curriculum.getCcno();
-		String host = curriculum.getHost();
+		String host = req.getParameter("host");
 		String writer = req.getParameter("writer");
 		
-		if(host.equals(writer)) {
-			Commons.printMsg("자신의 강좌를 수강 신청 할 수 없습니다.", "view?cbno=" + req.getParameter("cbno"), resp);
+		if(myClassService.findBy(ccno, writer) != null) {
+			Commons.printMsg("이미 신청한 클래스입니다.", "view?cbno=" + cbno, resp);
 			return;
 		}
 		
-		ClassReg classReg = ClassReg.builder()
-						.content(content)
-						.nickname(nickname)
-						.tel(tel)
-						.writer(writer)
-						.ccno(ccno)
-						.build();
+		if(host.equals(writer)) {
+			Commons.printMsg("자신의 강좌를 수강 신청 할 수 없습니다.", "view?cbno=" + cbno, resp);
+			return;
+		}
+		
+		ClassReg classReg = Commons.param(req, ClassReg.class);
 		System.out.println(classReg);
 		regService.write(classReg);
 		
@@ -75,4 +85,5 @@ public class RegisterClass extends HttpServlet {
 		
 		resp.sendRedirect("view?cbno=" + req.getParameter("cbno"));
 	}
+	
 }
