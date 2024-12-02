8f365a4b1cff0afa8746f67fbc586354a44f5b37
jungyoonn <sophia7625@naver.com>
2024-12-02 19:14:25 +0900

    이미지 업로드를 위한 vo 수정(attach List 추가)

--- src/main/java/vo/BoardClass.java
+++ src/main/java/vo/BoardClass.java
@@ -1,6 +1,8 @@
 package vo;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import lombok.*;
 
@@ -24,4 +26,27 @@ public class BoardClass {
 	private String dutime;
 	
 	private double avgStar; 
+	
+	@Builder.Default
+	private List<Attach> attachs = new ArrayList<Attach>();
+	public BoardClass(Long cbno, String title, String content, String host, Long viewCount, Date regdate,
+			Date updatedate, boolean end, int accept, int max, int price, String location, String dutime,
+			double avgStar) {
+		super();
+		this.cbno = cbno;
+		this.title = title;
+		this.content = content;
+		this.host = host;
+		this.viewCount = viewCount;
+		this.regdate = regdate;
+		this.updatedate = updatedate;
+		this.end = end;
+		this.accept = accept;
+		this.max = max;
+		this.price = price;
+		this.location = location;
+		this.dutime = dutime;
+		this.avgStar = avgStar;
+	}
+	
 }
--- src/main/java/vo/Review.java
+++ src/main/java/vo/Review.java
@@ -1,6 +1,9 @@
 package vo;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
 import lombok.*;
 
 @Data
@@ -15,4 +18,19 @@ public class Review {
 	private Date regdate;
 	private Date updatedate;
 	private Long ccno;
+	
+	@Builder.Default
+	private List<Attach> attachs = new ArrayList<Attach>();
+	public Review(Long reno, String content, String writer, int score, Date regdate, Date updatedate, Long ccno) {
+		super();
+		this.reno = reno;
+		this.content = content;
+		this.writer = writer;
+		this.score = score;
+		this.regdate = regdate;
+		this.updatedate = updatedate;
+		this.ccno = ccno;
+	}
+	
+	
 }
